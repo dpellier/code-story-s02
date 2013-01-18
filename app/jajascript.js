5b25aa8db991ef2a268730dc6a54886da05004e4
dpellier@gmail.com <>
2013-01-18 16:35:15 +0000

    Fix some bug on jajascript

--- app/jajascript.js
+++ app/jajascript.js
@@ -1,8 +1,11 @@
 var FLIES = [];
 
-var optimize = function(data) {
+/**
+ * Main method
+ */
+var optimize = function() {
   //var flies = JSON.parse(data);
-  data = [
+  var data = [
         { "VOL": "MONAD42", "DEPART": 0, "DUREE": 5, "PRIX": 10 },
         { "VOL": "META18", "DEPART": 3, "DUREE": 7, "PRIX": 14 },
         { "VOL": "LEGACY01", "DEPART": 5, "DUREE": 9, "PRIX": 8 },
@@ -23,28 +26,29 @@ var optimize = function(data) {
   
   // Make a search for each fly in parallel
   async.parallel(researches, function(err, result) {
-    // final result
+
     console.log("Fin : " + result);
   });
 }
 
-// Get all path from one specific fly
+/**
+ * Get all path from one specific fly
+ */
 var getPath = function(startFly, callback) {
-  
-  var startTime = startFly.DEPART + startFly.DUREE,
-      nextFlies = filterFlies(startTime),
-      possiblePaths = [];
-  
-  for (i in nextFlies) {
-    possiblePaths.push(getDeepPath(nextFlies[i], startTime, []));
-  }
+  console.log("Get Path fly : " + startFly.VOL);
+
+  possiblePaths = getDeepPath(startFly, startFly.DEPART, []);
+  console.log(startFly.VOL + " -> " + possiblePaths.toString());
   
   callback(possiblePaths);
 }
 
+/**
+ * Recursive function to get each path from a starting fly
+ */
 var getDeepPath = function(startFly, startTime, fullPaths) {
   var newStart = startTime + startFly.DUREE,
-      nextFlies = filterFlies(E);
+      nextFlies = filterFlies(newStart);
   
   // If we found some other matching fly
   if (nextFlies.length) {
@@ -53,10 +57,7 @@ var getDeepPath = function(startFly, startTime, fullPaths) {
     }
     
     // Add the fly value to all flies solutions
-    fullPaths.map(function(path) {
-      path.gain += startFly.PRIX;
-      path.path.push(startFly.DUREE);
-    });
+    fullPaths = addFlyValueToArray(startFly, fullPaths);
   } else {
     // We reach the end of the search we add the new path to the list
     fullPaths.push({gain: startFly.PRIX, path: [startFly.VOL]});
@@ -67,14 +68,22 @@ var getDeepPath = function(startFly, startTime, fullPaths) {
 
 // Return the best gain of each possible paths
 var filterBestPath = function(possiblePaths) {
-  return possiblePaths.sort(function(a, b) {
-    a.gain - b.gain;
-  })[0];
+  //return possiblePaths.sort(function(a, b) { return b.gain - a.gain; })[0];
+  console.log("toto");
 }
 
 // Keep only possible match
 var filterFlies = function(startValue) {
-  return FLIES.filter(function(next) { next.DEPART >= startValue; });
+  return FLIES.filter(function(next) { return next.DEPART >= startValue; });
+}
+
+// Add the fly value to all path solutions
+var addFlyValueToArray = function(fly, array) {
+  return array.map(function(path) {
+    path.gain += fly.PRIX;
+    path.path.push(fly.VOL);
+    return path;
+  });
 }
 
 // Create a function with callback to pass to the parallel call
