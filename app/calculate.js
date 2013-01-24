34d277dbe5466e96d42c76028bbe32d470becc33
dpellier@gmail.com <>
2013-01-24 10:38:13 +0000

    try

--- app/calculate.js
+++ app/calculate.js
@@ -6,10 +6,15 @@ var bigdecimal = require("bigdecimal");
 var calculate = function(expr) {
   expr = expr.replace(/\s/g, "+").replace(/,/g, ".");
  
-    var res = new bigdecimal.BigDecimal(eval(expr)).stripTrailingZeros();
+    var res = new bigdecimal.BigDecimal(eval(expr));
     // shame on me
     //if (res > 100000) return "31878018903828899277492024491376690701584023926880"
-    return res.toString().replace(/\./g, ",");
+    
+    if (res.toString().indexOf(".") < 0) {
+      return res.toBigInteger().toString();
+    }
+    
+    return res.stripTrailingZeros().toString().replace(/\./g, ",");
 }
 
 exports.calculate = calculate;
